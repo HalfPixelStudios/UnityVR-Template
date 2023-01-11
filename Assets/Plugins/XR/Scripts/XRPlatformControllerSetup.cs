6343199a62f9263389f6171ffd32b3aef6fde311
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-01-10 21:54:26 -0500

    vscode autoformatting

--- Assets/Plugins/XR/Scripts/XRPlatformControllerSetup.cs
+++ Assets/Plugins/XR/Scripts/XRPlatformControllerSetup.cs
@@ -7,40 +7,35 @@ using UnityEditor.XR.Management;
 using UnityEngine.XR.Management;
 #endif
 
-namespace Unity.Template.VR
-{
-    internal class XRPlatformControllerSetup : MonoBehaviour
-    {
-        [SerializeField]
-        GameObject m_LeftController;
+namespace Unity.Template.VR {
+  internal class XRPlatformControllerSetup : MonoBehaviour {
+    [SerializeField]
+    GameObject m_LeftController;
 
-        [SerializeField]
-        GameObject m_RightController;
-        
-        [SerializeField]
-        GameObject m_LeftControllerOculusPackage;
+    [SerializeField]
+    GameObject m_RightController;
 
-        [SerializeField]
-        GameObject m_RightControllerOculusPackage;
+    [SerializeField]
+    GameObject m_LeftControllerOculusPackage;
 
-        void Start()
-        {
+    [SerializeField]
+    GameObject m_RightControllerOculusPackage;
+
+    void Start() {
 #if UNITY_EDITOR
-            var loaders = XRGeneralSettingsPerBuildTarget.XRGeneralSettingsForBuildTarget(BuildTargetGroup.Standalone).Manager.activeLoaders;
+      var loaders = XRGeneralSettingsPerBuildTarget.XRGeneralSettingsForBuildTarget(BuildTargetGroup.Standalone).Manager.activeLoaders;
 #else
             var loaders = XRGeneralSettings.Instance.Manager.activeLoaders;
 #endif
-            
-            foreach (var loader in loaders)
-            {
-                if (loader.name.Equals("Oculus Loader"))
-                {
-                    m_RightController.SetActive(false);
-                    m_LeftController.SetActive(false);
-                    m_RightControllerOculusPackage.SetActive(true);
-                    m_LeftControllerOculusPackage.SetActive(true);
-                }
-            }
+
+      foreach (var loader in loaders) {
+        if (loader.name.Equals("Oculus Loader")) {
+          m_RightController.SetActive(false);
+          m_LeftController.SetActive(false);
+          m_RightControllerOculusPackage.SetActive(true);
+          m_LeftControllerOculusPackage.SetActive(true);
         }
+      }
     }
+  }
 }
--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 </div>
 
 Template project for Unity VR.
+
+# Getting setup for Unity Development (VSCode)
+
+First install the [.NET sdk (7.0)](https://dotnet.microsoft.com/en-us/download). As well as the Visual Studio [Build Tools](https://visualstudio.microsoft.com/downloads/#build-tools-for-visual-studio-2022).
+
+Next install the following VSCode extensions
+- [C#](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp)
+
